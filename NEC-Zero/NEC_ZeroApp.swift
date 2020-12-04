7edef6e7659d1610036b559879d7ed4344a7118e
David Celaya-Gonzalez <djcelaya@icloud.com>
2020-12-04 16:21:07 -0700

    Add blank Xcode project

---
+++ NEC-Zero/NEC_ZeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEC_ZeroApp.swift
+//  NEC-Zero
+//
+//  Created by David Celaya on 12/4/20.
+//
+
+import SwiftUI
+
+@main
+struct NEC_ZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
