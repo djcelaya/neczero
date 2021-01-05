7967166618ff4306855b74631e7873ca661ef1b7
David Celaya-Gonzalez <djcelaya@icloud.com>
2021-01-05 16:41:42 -0700

    Beginning GutCheckNEC model

---
+++ NEC-Zero/GutCheckNEC.swift
@@ -0,0 +1,114 @@
+//
+//  GutCheckNEC.swift
+//  NEC-Zero
+//
+//	Model for GutCheckNEC risk measurement tool
+//
+//  Created by David Celaya-Gonzalez on 1/5/21.
+//
+
+import Foundation
+
+struct GutCheckNEC {
+
+	var points = 0
+
+	let gestationAge: Double // weeks
+
+	let race: Race
+
+	let outborn: Bool
+
+	let necRate: Double // percentage
+
+	let humanMilkFeeding: Bool
+
+	let probiotics: Bool
+
+	let infections: Int
+
+	let prbcTransfusion: Bool
+
+	let hypotension: Bool
+
+	let metabolicAcidosis: Bool
+
+	init(gestationAge: Double,
+		 race: Race,
+		 outborn: Bool,
+		 necRate: Double,
+		 humanMilkFeeding: Bool,
+		 probiotics: Bool,
+		 infections: Int,
+		 prbcTransfusion: Bool,
+		 hypotension: Bool,
+		 metabolicAcidosis: Bool) {
+		self.gestationAge = gestationAge
+		self.race = race
+		self.outborn = outborn
+		self.necRate = necRate
+		self.humanMilkFeeding = humanMilkFeeding
+		self.probiotics = probiotics
+		self.infections = infections
+		self.prbcTransfusion = prbcTransfusion
+		self.hypotension = hypotension
+		self.metabolicAcidosis = metabolicAcidosis
+
+		points += points(for: gestationAge)
+		points += points(for: race)
+	}
+
+	private func points(for gestationAge: Double) -> Int {
+		if gestationAge < 28 {
+			return 9
+		} else if gestationAge < 32 {
+			return 8
+		} else { // >= 32
+			return 0
+		}
+	}
+
+	private func points(for race: Race) -> Int {
+		switch race {
+			case .black:
+				return 2
+			case .hispanic:
+				return 2
+			case .other:
+				return 0
+		}
+	}
+
+	private func points(for outborn: Bool) -> Int {
+		return outborn ? 3 : 0
+	}
+
+//	private func points(for necRate: Double) -> Int {
+//		if necRate < 2 {
+//			
+//		}
+//	}
+
+}
+
+extension GutCheckNEC {
+
+	enum GestationAge {
+		case ageRange1
+		case ageRange2
+		case ageRange3
+	}
+
+	enum Race {
+		case black
+		case hispanic
+		case other
+	}
+
+	enum NECRate {
+		case rateInterval1
+		case rateInterval2
+		case rateInterval3
+		case rateInterval4
+	}
+}
