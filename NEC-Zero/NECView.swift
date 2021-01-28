c860f541404b1b621c48f09edc338f3ec1301f3c
David Celaya-Gonzalez <djcelaya@icloud.com>
2021-01-28 07:54:37 -0700

    Add strings files for EN and ES languages

--- NEC-Zero/NECView.swift
+++ NEC-Zero/NECView.swift
@@ -17,21 +17,21 @@ struct NECView: View {
                 Button(action: {
 
                 }) {
-                    Text("Read about the NEC-Zero project")
+                    Text("what-is-nec-0")
                         .padding([.leading])
                 }
-                Text("What is NEC?")
+                Text("what-is-nec-1")
                     .font(.title)
                     .padding([.leading, .trailing, .top])
-                Text("Necrotizing enterocolitis (nek-roh-TIE-zing en-ter-oh-coh-LIE-tis), also called NEC (often pronounced as “neck”), is an infection that in many cases is followed by redness or swelling of the intestines (bowel).")
+                Text("what-is-nec-2")
                     .padding([.leading, .trailing, .top])
-                Text("This infection can cause damage to the bowel over time, which can lead to the death of portions of the bowel. In some cases, a hole can form in the bowel, known as a perforation. If the bowel breaks open, it can cause the infection to spread throughout the body.")
+                Text("what-is-nec-3")
                     .padding([.leading, .trailing, .top])
-                Text("Often NEC impacts babies who are born very early and very small. Being born early can increase the risk of NEC because many organs have not yet fully developed and their immune systems are immature. Infants who develop NEC are typically preterm, which means they are born before 32 weeks of pregnancy (gestation). However, NEC can also occur in babies who are born at 37 weeks (full term) of pregnancy.")
+                Text("what-is-nec-4")
                     .padding([.leading, .trailing, .top])
-                Text("The chance of your baby developing NEC is low. However, NEC is serious and sometimes causes death.")
+                Text("what-is-nec-5")
                     .padding([.leading, .trailing, .top])
-                Text("Sometimes parents and family members are the most aware of changes in their babies’ normal habits. As a parent, it is important to be watchful for some of the signs and symptoms that could mean your baby might be ill.")
+                Text("what-is-nec-6")
                     .padding([.leading, .trailing, .top])
             }
             Spacer()
@@ -41,6 +41,15 @@ struct NECView: View {
 
 struct NECView_Previews: PreviewProvider {
     static var previews: some View {
-        NECView()
+        ForEach(["en", "es"], id: \.self) { id in
+            TabView {
+                NECView().tabItem {
+                    VStack {
+                        Image(systemName: "heart")
+                        Text("NEC")
+                    }
+                }.tag("NEC")
+            }.environment(\.locale, .init(identifier: id))
+        }
     }
 }
