62f320231b785446c89e1dbfb06c981ee5f8261f
David Celaya-Gonzalez <djcelaya@icloud.com>
2021-02-17 09:25:37 -0700

    GutCheckNEC form can switch between quick and guided versions

--- NEC-Zero/GutCheckNECForm.swift
+++ NEC-Zero/GutCheckNECForm.swift
@@ -10,53 +10,102 @@ import SwiftUI
 struct GutCheckNECForm: View {
 
     @ObservedObject var viewModel: GutCheckNECViewModel
+    @State private var displayQuickForm = false
+    @State private var questionIndex = 0
+    private let backgroundGradient = Color("PrimaryColor")
 
     var body: some View {
-        NavigationView {
-            Form {
-                Section(header: Text("GutChecNEC (< 1500 grams)")) {
-                    Group {
-                        Picker(selection: $viewModel.gestationalAgeIndex, label: Text("Gestational age (weeks)")) {
-                            ForEach(0 ..< viewModel.gestationalAgeOptions.count) {
-                                Text(viewModel.gestationalAgeOptions[$0])
-                            }
-                        }
-                        Picker("Race", selection: $viewModel.raceIndex) {
-                            ForEach(0 ..< viewModel.raceOptions.count) {
-                                Text(viewModel.raceOptions[$0])
-                            }
-                        }
-                        Toggle("Outborn", isOn: $viewModel.outborn)
-                        Picker("NICU NEC Rate", selection: $viewModel.necRateIndex) {
-                            ForEach(0 ..< viewModel.necRateOptions.count) {
-                                Text(viewModel.necRateOptions[$0])
-                            }
-                        }
-                        Toggle("Exclusive human milk feeding", isOn: $viewModel.milkFeeding)
-                        Toggle("Probiotics", isOn: $viewModel.probiotics)
-                        Picker(selection: $viewModel.infectionsIndex, label:
-                                Text("How many culture-proven infections has the infant had since day 3 of life?")) {
-                            ForEach(0 ..< viewModel.infectionsOptions.count) {
-                                Text(viewModel.infectionsOptions[$0])
-                            }
-                        }
-                        Toggle("Packed Red Blood Cell transfusion history", isOn: $viewModel.prbcTransfusion)
-                        Toggle("Hypotension treated with Inotropic Medication", isOn: $viewModel.hypotension)
-                        Toggle("Metabolic Acidosis", isOn: $viewModel.metabolicAcidosis)
-                    }
-                    Button("Submit", action: viewModel.submit)
+        Group {
+            if displayQuickForm {
+                QuickForm()
+            } else {
+                GuidedForm()
+            }
+        }
+        .navigationBarTitle("GutCheckNEC")
+        .navigationBarItems(
+            trailing:
+                Button("Switch") {
+                    displayQuickForm = !displayQuickForm
                 }
-                Section(header: Text("Results")) {
-                    HStack {
-                        Text("Points")
-                        Spacer()
-                        Text("\(viewModel.points)")
+        )
+    }
+
+    func GuidedForm() -> some View {
+        TabView(selection: $questionIndex) {
+            QuestionCard()
+                .tag(0)
+            Text("Race")
+                .tag(1)
+            Text("Outborn")
+                .tag(2)
+        }
+        .background(backgroundGradient)
+        .tabViewStyle(PageTabViewStyle())
+        .animation(.easeInOut)
+    }
+
+    func QuestionCard() -> some View {
+        VStack {
+            Text("Gestational age (weeks)")
+            Text("Calculate GA in weeks at birth based preferably on due date determined by 1st trimester ultrasound. If that is unavailable, use the gestational age assessment at delivery (Ballard or Dubowitz)")
+            Button("<28") {
+
+            }
+            Button("28-31 6/7") {
+
+            }
+            Button(">= 32") {
+
+            }
+        }
+        .background(Color.white)
+        .cornerRadius(20)
+        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+
+    func QuickForm() -> some View {
+        Form {
+            Section(header: Text("GutChecNEC (< 1500 grams)")) {
+                Group {
+                    Picker(selection: $viewModel.gestationalAgeIndex, label: Text("Gestational age (weeks)")) {
+                        ForEach(0 ..< viewModel.gestationalAgeOptions.count) {
+                            Text(viewModel.gestationalAgeOptions[$0])
+                        }
                     }
+                    Picker("Race", selection: $viewModel.raceIndex) {
+                        ForEach(0 ..< viewModel.raceOptions.count) {
+                            Text(viewModel.raceOptions[$0])
+                        }
+                    }
+                    Toggle("Outborn", isOn: $viewModel.outborn)
+                    Picker("NICU NEC Rate", selection: $viewModel.necRateIndex) {
+                        ForEach(0 ..< viewModel.necRateOptions.count) {
+                            Text(viewModel.necRateOptions[$0])
+                        }
+                    }
+                    Toggle("Exclusive human milk feeding", isOn: $viewModel.milkFeeding)
+                    Toggle("Probiotics", isOn: $viewModel.probiotics)
+                    Picker(selection: $viewModel.infectionsIndex, label:
+                            Text("How many culture-proven infections has the infant had since day 3 of life?")) {
+                        ForEach(0 ..< viewModel.infectionsOptions.count) {
+                            Text(viewModel.infectionsOptions[$0])
+                        }
+                    }
+                    Toggle("Packed Red Blood Cell transfusion history", isOn: $viewModel.prbcTransfusion)
+                    Toggle("Hypotension treated with Inotropic Medication", isOn: $viewModel.hypotension)
+                    Toggle("Metabolic Acidosis", isOn: $viewModel.metabolicAcidosis)
+                }
+                Button("Submit", action: viewModel.submit)
+            }
+            Section(header: Text("Results")) {
+                HStack {
+                    Text("Points")
+                    Spacer()
+                    Text("\(viewModel.points)")
                 }
             }
-            .navigationBarTitle("GutCheckNEC")
         }
-
     }
 
     init(with viewModel: GutCheckNECViewModel = GutCheckNECViewModel(with: GutCheckNEC())) {
@@ -69,7 +118,9 @@ struct GutCheckNECView_Previews: PreviewProvider {
         let model = GutCheckNEC()
         let viewModel = GutCheckNECViewModel(with: model)
         return TabView {
-            GutCheckNECForm(with: viewModel).tabItem {
+            NavigationView {
+                GutCheckNECForm(with: viewModel)
+            }.tabItem {
                 VStack {
                     Image(systemName: "heart.text.square")
                     Text("GutCheckNEC")
