79490c2a221c3c56e504a6d9622859b444a53981
David Celaya-Gonzalez <djcelaya@icloud.com>
2021-02-18 12:51:10 -0700

    Moved QuestionCard into its own view

--- NEC-Zero/GutCheckNECForm.swift
+++ NEC-Zero/GutCheckNECForm.swift
@@ -45,30 +45,6 @@ struct GutCheckNECForm: View {
         .animation(.easeInOut)
     }
 
-    func QuestionCard() -> some View {
-        VStack(alignment: .center, spacing: 16) {
-            Text("Gestational age (weeks)")
-                .font(.title2)
-                .padding(.top)
-                .padding(.horizontal)
-            Text("Calculate GA in weeks at birth based preferably on due date determined by 1st trimester ultrasound. If that is unavailable, use the gestational age assessment at delivery (Ballard or Dubowitz)")
-                .multilineTextAlignment(.center)
-                .padding(.horizontal)
-            VStack {
-                QuestionResponseButton("<28")
-                Button("28-31 6/7") {
-
-                }
-                Button(">= 32") {
-
-                }
-            }
-        }
-        .background(Color.white)
-        .cornerRadius(20)
-        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-    }
-
     func QuickForm() -> some View {
         Form {
             Section(header: Text("GutChecNEC (< 1500 grams)")) {
@@ -118,24 +94,58 @@ struct GutCheckNECForm: View {
     }
 }
 
+struct QuestionCard: View {
+    @State var option1Selected = false
+    @State var option2Selected = false
+    @State var option3Selected = false
+    var body: some View {
+        VStack(alignment: .center, spacing: 16) {
+            Text("Gestational age (weeks)")
+                .font(.title2)
+                .padding(.top)
+                .padding(.horizontal)
+            Text("Calculate GA in weeks at birth based preferably on due date determined by 1st trimester ultrasound. If that is unavailable, use the gestational age assessment at delivery (Ballard or Dubowitz)")
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+            VStack(spacing: 10) {
+                QuestionResponseButton("<28", selected: $option1Selected)
+                QuestionResponseButton("28-31 6/7", selected: $option2Selected)
+                QuestionResponseButton(">= 32", selected: $option3Selected)
+            }
+            .padding(.bottom)
+        }
+        .background(Color.white)
+        .cornerRadius(20)
+        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+}
+
 struct QuestionResponseButton: View {
 
     private let responseText: LocalizedStringKey
+    @Binding var isSelected: Bool
 
     var body: some View {
-        Button(responseText) {
+        HStack {
+            if isSelected {
+                Image(systemName: "checkmark")
+            }
+            Button(responseText) {
 
+            }
+            .padding()
+            .background(Color("AccentColor"))
+            .font(.title3)
+            .foregroundColor(.white)
+            .cornerRadius(24)
+            .shadow(radius: 8)
         }
-        .padding()
-        .background(Color("AccentColor"))
-        .font(.title3)
-        .foregroundColor(.white)
-        .cornerRadius(24)
-        .shadow(radius: 8)
     }
 
-    init(_ responseText: LocalizedStringKey) {
+    // https://stackoverflow.com/questions/56973959/swiftui-how-to-implement-a-custom-init-with-binding-variables
+    init(_ responseText: LocalizedStringKey, selected: Binding<Bool>) {
         self.responseText = responseText
+        self._isSelected = selected
     }
 }
 
