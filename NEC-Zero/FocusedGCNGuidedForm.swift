50a2cc74335bcbfe28ee4ba63d3bd28364a1c353
David Celaya-Gonzalez <djcelaya@icloud.com>
2021-03-12 13:27:07 -0700

    String keys (EN) for remaining Focused GCN questions

--- NEC-Zero/FocusedGCNGuidedForm.swift
+++ NEC-Zero/FocusedGCNGuidedForm.swift
@@ -68,6 +68,7 @@ struct FocusedGCNGuidedForm: View {
         .background(Color.white)
         .cornerRadius(20)
         .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+        .padding()
     }
 
     func advance() {
@@ -76,12 +77,6 @@ struct FocusedGCNGuidedForm: View {
 
     init(with viewModel: FocusedGCNViewModel = FocusedGCNViewModel()) {
         self.viewModel = viewModel
-//        questionIndices = [
-//            viewModel.gestationAgeTitle,
-//            viewModel.raceTitle,
-//            viewModel.outbornTitle
-//        ]
-//        _questionIndex = State(initialValue: viewModel.gestationAgeTitle)
         _questionIndex = State(initialValue: 0)
     }
 }
@@ -112,90 +107,6 @@ extension View {
         return self.modifier(OptionButtonStyle(selected: selected))
     }
 }
-
-//struct QuestionCard: View {
-//    let title: LocalizedStringKey
-//    private(set) var description: LocalizedStringKey?
-//    var responses: [FocusedGCNViewModel.Question.Response]
-//
-//    @State var option1Selected = false
-//    @State var option2Selected = false
-//    @State var option3Selected = false
-//    var body: some View {
-//        VStack(alignment: .center, spacing: 16) {
-//            Text(title)
-//                .font(.title2)
-//                .padding(.top)
-//                .padding(.horizontal)
-//            Text(description ?? "")
-//                .multilineTextAlignment(.center)
-//                .padding(.horizontal)
-//            VStack(spacing: 10) {
-////                QuestionResponseButton("<28", selected: $option1Selected)
-////                QuestionResponseButton("28-31 6/7", selected: $option2Selected)
-////                QuestionResponseButton(">= 32", selected: $option3Selected)
-//                ForEach(responses) { response in
-////                    QuestionResponseButton(response.displayValue, selected: response.isSelected)
-////                    QuestionResponseButton(for: response)
-//                }
-//            }
-//            .padding(.bottom)
-//        }
-//        .background(Color.white)
-//        .cornerRadius(20)
-//        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-//    }
-//
-//    init(for question: FocusedGCNViewModel.Question) {
-//        title = LocalizedStringKey(stringLiteral: question.title)
-//        if let description = question.description {
-//            self.description = LocalizedStringKey(stringLiteral: description)
-//        }
-//        responses = question.responses
-//    }
-//}
-
-//struct QuestionResponseButton: View {
-//
-//    private let responseText: LocalizedStringKey
-//    @Binding var isSelected: Bool
-//    //var response: FocusedGCNViewModel.Question.Response
-//
-//    var body: some View {
-//        HStack {
-//            if isSelected {
-//                Image(systemName: "checkmark")
-//            }
-//            Button(responseText) {
-//                isSelected = true
-//            }
-//            .padding()
-//            .background(Color("AccentColor"))
-//            .font(.title3)
-//            .foregroundColor(.white)
-//            .cornerRadius(24)
-//            .shadow(radius: 8)
-//        }
-//    }
-//
-//    // https://stackoverflow.com/questions/56973959/swiftui-how-to-implement-a-custom-init-with-binding-variables
-////    init(_ responseText: LocalizedStringKey, selected: Binding<Bool>) {
-////        self.responseText = responseText
-////        self._isSelected = selected
-////    }
-//
-//    init(_ responseText: String, selected: Binding<Bool>) {
-//        self.responseText = LocalizedStringKey(stringLiteral: responseText)
-//        _isSelected = selected
-//    }
-//
-////    init(for response: FocusedGCNViewModel.Question.Response) {
-////        responseText = LocalizedStringKey(stringLiteral: response.displayValue)
-////        _isSelected = State(initialValue: response.isSelected)
-////        self.response = response
-////    }
-//}
-
 struct FocusedGCNGuidedForm_Previews: PreviewProvider {
     static var previews: some View {
         TabView {
