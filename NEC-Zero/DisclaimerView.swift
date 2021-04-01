fdf785dcc9c92d6beba8e90e8b0cd7fb7999c10d
David Celaya-Gonzalez <djcelaya@icloud.com>
2021-04-01 10:03:30 -0700

    Disclaimer presents an accept button and stores response in UserDefaults

--- NEC-Zero/DisclaimerView.swift
+++ NEC-Zero/DisclaimerView.swift
@@ -2,19 +2,55 @@
 //  DisclaimerView.swift
 //  NEC-Zero
 //
-//  Created by David Celaya on 3/29/21.
+//  Created by David Celaya-Gonzalez on 3/29/21.
 //
 
 import SwiftUI
 
 struct DisclaimerView: View {
+
+    @State private var hasAcceptedDisclaimer = UserDefaults.standard.bool(forKey: "Disclaimer")
+    @State private var dateAccepted: Date?
+
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
                 Text("The material provided in this mobile application is for...")
                     .padding()
+                Button(action: {
+                    hasAcceptedDisclaimer = true
+                    UserDefaults.standard.setValue(hasAcceptedDisclaimer, forKey: "Disclaimer")
+                    dateAccepted = Date()
+                    UserDefaults.standard.setValue(dateAccepted, forKey: "DisclaimerDate")
+                }, label: {
+                    VStack(alignment: .leading) {
+                        Text("I have read and accept the disclaimer above.")
+                        if hasAcceptedDisclaimer {
+                            if let date = dateAccepted {
+                                Text("Disclaimer accepted on \(date.asString())")
+                            }
+                        }
+                    }
+                })
+                .disabled(hasAcceptedDisclaimer) // once accepted, cannot unaccept
+                .padding()
+//                .background(Color(red: 211/255, green: 211/255, blue: 211/255, opacity: 1))
             }
         }.navigationTitle("Disclaimer")
+    }
+
+    init() {
+        if let date = UserDefaults.standard.object(forKey: "DisclaimerDate") as! Date? {
+            _dateAccepted = State(initialValue: date)
+        }
+    }
+}
+
+extension Date {
+    func asString() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        return dateFormatter.string(from: self)
     }
 }
 
@@ -25,7 +61,6 @@ struct DisclaimerView_Previews: PreviewProvider {
                 DisclaimerView()
             }.tabItem {
                 VStack {
-
                     Image(systemName: "list.bullet")
                     Text("More")
                 }
