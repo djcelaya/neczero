8cfda78422d48511aa1af85c5fef44a10542820b
David Celaya-Gonzalez <djcelaya@icloud.com>
2021-04-06 11:24:59 -0700

    BroadGCN model with properties

---
+++ NEC-Zero/BroadGCN.swift
@@ -0,0 +1,93 @@
+//
+//  BroadGCN.swift
+//  NEC-Zero
+//
+//  Model for broad GutCheckNEC (GCN) risk assessment tool
+//
+//  Created by David Celaya-Gonzalez on 4/5/21.
+//
+
+import Foundation
+
+struct BroadGCN {
+
+    // G
+    var growthRestricted: Bool?
+    var weight: Int? // grams
+    var age: Double? // weeks
+    var glucocorticoids: Bool?
+
+    // U
+    var umbilicalAcidic: Bool?
+    var umbilicalProlapse: Bool?
+
+    // T
+    var transfusion: Bool?
+    enum NICUCharacteristics {
+        case lowAcuityVolume
+        case mediumAcuityVolume
+        case highAcuityVolume
+    }
+    var nicuCharacteristics: NICUCharacteristics?
+    var antenatalSteroids: Bool?
+    var standardizedFeeding: Bool?
+    var milk: Bool?
+    var probiotics: Bool?
+
+    // C
+    var coldStress: Bool?
+    var cocaine: Bool?
+    var placentalAbruption: Bool?
+    var apgar: Bool?
+    var chestCompressions: Bool?
+    var epinephrine: Bool?
+    var necRate: Double?
+
+    // H
+    var histamine2: Bool?
+    var resusication: Bool?
+    var hypertensive: Bool?
+
+    // E
+    enum EnteralFeeding {
+        case allFormula
+        case formulaHuman
+        case bovineFortified
+    }
+    var enteralFeeding: EnteralFeeding?
+
+    // C
+    var heartDisease: Bool?
+    var pdaSurgical: Bool?
+    var pdaHemodynamic: Bool?
+    var pdaIndomethacin: Bool?
+    var multipleInfections: Bool?
+    var tripleAntibiotic: Bool?
+    var antibioticCourse: Bool?
+    var sepsisEarly: Bool?
+    var sepsisLate: Bool?
+    var chorioamnionitis: Bool?
+
+    // K
+    var points: Int? { pointsForChoices() }
+
+    func pointsForChoices() -> Int? {
+        guard let growthRestricted = self.growthRestricted,
+              let weight = self.weight,
+              let age = self.age,
+              let glucocorticoids = self.glucocorticoids,
+              let umbilicalAcidic = self.umbilicalAcidic,
+              let umbilicalProlapse = self.umbilicalProlapse,
+              let tranfusion = self.transfusion,
+              let nicuCharacteristics = self.nicuCharacteristics,
+              let antenatalSteroids = self.antenatalSteroids,
+              let standardizedFeeding = self.standardizedFeeding,
+              let milk = self.milk,
+              let probiotics = self.probiotics else {
+            return nil
+        }
+
+        var total = 0
+        return total
+    }
+}
