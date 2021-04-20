234986362836b1146b36d1b2a02a8674abb862c3
David Celaya-Gonzalez <djcelaya@icloud.com>
2021-04-19 17:37:53 -0700

    Trying out some "protected" view generalizations - none work

---
+++ NEC-Zero/ProtectedView.swift
@@ -0,0 +1,48 @@
+//
+//  ProtectedView.swift
+//  NEC-Zero
+//
+//  A view container that serves as a type of middleware protection. The ProtectedView shows
+//
+//  Created by David Celaya-Gonzalez on 4/19/21.
+//
+
+import SwiftUI
+
+//struct ProtectedView<Content>: View where Content: View {
+//
+//    var hasAcceptedDisclaimer: Bool { UserDefaults.standard.bool(forKey: "Disclaimer") }
+//    @State var isPresentingDisclaimer = false
+//    var content: Content
+//
+//    var body: some View {
+//        content
+//            .sheet(isPresented: $isPresentingDisclaimer, content: {
+//                DisclaimerView($isPresentingDisclaimer)
+//            })
+//    }
+//
+//    func shouldPresentDisclaimer() -> Bool {
+//        if hasAcceptedDisclaimer {
+//            return false
+//        }
+//    }
+//
+//    init(content: @escaping () -> Content) {
+//        self.content = content()
+//    }
+//}
+//
+//struct ProtectedView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ProtectedView {
+//            Text("Hello, World!")
+//        }
+//    }
+//}
+
+struct Protected: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+    }
+}
