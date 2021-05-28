5c1db4c76a505bebaa9b82588b15cef48b36255f
David Celaya-Gonzalez <djcelaya@icloud.com>
2021-05-28 09:34:08 -0700

    EN text for NECSymptomsView

---
+++ NEC-Zero/NECSymptomsView.swift
@@ -0,0 +1,51 @@
+//
+//  NECSymptomsView.swift
+//  NEC-Zero
+//
+//  Corresponding webpage: https://neczero.nursing.arizona.edu/nec-symptoms
+//
+//  Created by David Celaya-Gonzalez on 5/28/21.
+//
+
+import SwiftUI
+
+struct NECSymptomsView: View {
+
+    let columns = [
+        GridItem(.fixed(20), alignment: .topLeading),
+        GridItem(.flexible(), alignment: .topLeading)
+    ]
+
+    var body: some View {
+        ScrollView {
+            LazyVStack(alignment: .leading) {
+                Text("Although experts don’t agree...")
+                    .padding([.leading, .trailing])
+                LazyVGrid(columns: columns) {
+                    Text("•")
+                    Text("An immature immune system that responds...")
+                    Text("•")
+                    Text("A lack of good bacteria in...")
+                    Text("•")
+                    Text("A vulnerable or weak intestine (because of immaturity)")
+                    Text("•")
+                    Text("The use of formula instead of breast milk...")
+                    Text("•")
+                    Text("Lack of oxygen (seen in difficult deliveries or in babies with heart problems)")
+                }.padding([.leading, .trailing, .bottom])
+            }//.padding([.leading, .trailing])
+        }.navigationTitle("What are the symptoms of NEC?")
+    }
+}
+
+struct NECSymptomsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["en", "es"], id: \.self) { id in
+            TabView {
+                NavigationView() {
+                    NECSymptomsView()
+                }
+            }.environment(\.locale, .init(identifier: id))
+        }
+    }
+}
