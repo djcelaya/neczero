b28c8a7a48674dc9531c0b80aea09560a0b5e94f
David Celaya-Gonzalez <djcelaya@icloud.com>
2021-06-14 10:47:55 -0700

    Converting link list to a generic View 2

--- NEC-Zero/LinkList.swift
+++ NEC-Zero/LinkList.swift
@@ -13,25 +13,72 @@ import SwiftUI
 struct LinkList: View {
 
     var title: String?
-    var links: [Link<Text>]?
+//    let link1: () -> NECResourceLink
+//    var link2: (() -> NECResourceLink)?
+    var links2: (() -> TupleView<(NECResourceLink, NECResourceLink)>)?
 
     var body: some View {
         LazyVStack(alignment: .leading) {
             if let _title = title {
                 Text(_title)
+                    .font(.title2)
+                    .padding([.leading, .trailing])
             }
             Divider()
-
+//            link1()
+            Divider()
         }
     }
 
-    init(_ title: String?) {
+//    init(title: String?, _ link: @escaping () -> NECResourceLink) {
+//        self.title = title
+//        link1 = link
+//    }
+
+    init(title: String?, _ links: @escaping () -> TupleView<(NECResourceLink, NECResourceLink)>) {
+//        self.init(title: title, link)
+//        self.link2 = link2
         self.title = title
+        self.links2 = links
+    }
+
+}
+
+struct NECResourceLink: View {
+
+    let title: String
+    let url: String
+    var description: String?
+
+    var body: some View {
+        Link(destination: URL(string: url)!) {
+            VStack(alignment: .leading, spacing: 4) {
+                Text(title)
+                    .font(.title3)
+                if let _description = description {
+                    Text(_description)
+                        .foregroundColor(.black)
+                }
+            }.padding([.leading, .trailing])
+            Spacer()
+        }
     }
 }
 
 struct LinkList_Previews: PreviewProvider {
     static var previews: some View {
-        LinkList("Breastfeeding Resources")
+        LinkList(title: "Breastfeeding Resources") {
+            NECResourceLink(
+                title: "La Leche League",
+                url: "https://www.llli.org",
+                description: "Offers multi-language breastfeeding..."
+            )
+            NECResourceLink(
+                title: "MedlinePlus",
+                url: "https://medlineplus.gov/breastfeeding.html",
+                description: "Offers breastfeeding information in many...")
+//            Text("MedlinePLue")
+//            Text("Womenshealth.gov")
+        }
     }
 }
