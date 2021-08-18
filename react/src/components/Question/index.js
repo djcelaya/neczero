758c1252cfd7758cda2ae9b05f05c569e3500e36
David Celaya-Gonzalez <djcelaya@icloud.com>
2021-08-18 13:31:02 -0700

    Convert Question to function component

--- react/src/components/Question/index.js
+++ react/src/components/Question/index.js
@@ -1,35 +1,33 @@
 import React from 'react';
 import ResponseButton from '../ResponseButton';
 
-export default class Question extends React.Component {
-  render() {
-    let question = this.props.data;
-    let responses = question.responses;
-    let responseButtons = Object.keys(responses).map((responseID) => {
-      let response = question.responses[responseID];
-      return (
-        <ResponseButton
-          key={response.id}
-          text={response.text}
-          points={response.points}
-          handleClick={() => this.props.handleResponse(this.props.data.id, response.id)}
-          selected={this.props.isSelected(this.props.data.id, response.id)}
-        />
-      );
-    });
-    let numResponses = Object.keys(responses).length;
-    let questionColumnWidth = 12 - 1 - numResponses;
+export default function Question(props) {
+  let question = props.data;
+  let responses = question.responses;
+  let responseButtons = Object.keys(responses).map((responseID) => {
+    let response = question.responses[responseID];
     return (
-      <div className="row">
-        <div className={"col-" + questionColumnWidth + " border"}>
-          <h6>{this.props.data.title + ' ' + this.props.data.subtitle}</h6>
-          <p dangerouslySetInnerHTML={{__html: this.props.data.body}}></p>
-        </div>
-        {responseButtons}
-        <div className="col-1 border">
-          <p>{this.props.points}</p>
-        </div>
-      </div>
+      <ResponseButton
+        key={response.id}
+        text={response.text}
+        points={response.points}
+        handleClick={() => props.handleResponse(props.data.id, response.id)}
+        selected={props.isSelected(props.data.id, response.id)}
+      />
     );
-  }
+  });
+  let numResponses = Object.keys(responses).length;
+  let questionColumnWidth = 12 - 1 - numResponses;
+  return (
+    <div className="row">
+      <div className={"col-" + questionColumnWidth + " border"}>
+        <h6>{props.data.title + ' ' + props.data.subtitle}</h6>
+        <p dangerouslySetInnerHTML={{__html: props.data.body}}></p>
+      </div>
+      {responseButtons}
+      <div className="col-1 border">
+        <p>{props.points}</p>
+      </div>
+    </div>
+  );
 }
--- react/src/components/ResponseButton/index.js
+++ react/src/components/ResponseButton/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 export default function ResponseButton(props) {
-    let className = "col-1 border";
-    if (props.selected) {
-      className += " bg-info"
-    }
-    return (
-      <button className={className} onClick={() => props.handleClick()}>
-        <p dangerouslySetInnerHTML={{__html: props.text}}></p>
-        <p>{props.points} points</p>
-      </button>
-    );
+  let className = "col-1 border";
+  if (props.selected) {
+    className += " bg-info"
   }
+  return (
+    <button className={className} onClick={() => props.handleClick()}>
+      <p dangerouslySetInnerHTML={{__html: props.text}}></p>
+      <p>{props.points} points</p>
+    </button>
+  );
+}
